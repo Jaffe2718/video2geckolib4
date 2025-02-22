29a8f8565ed90ca195d3cea8cf4c75aa9cb3bccf
Jaffe2718 <qqyttwqeei@163.com>
2025-02-22 19:41:53 +0800

    add resource

--- setup.py
+++ setup.py
@@ -4,13 +4,16 @@ import os
 setup(
     name='video2geckolib4',
     version='0.1.0',
-    author='Jaffe2718',  # 替换为你的名字
-    author_email='qqyttwqeei@163.com',  # 替换为你的邮箱
+    author='Jaffe2718',
+    author_email='qqyttwqeei@163.com',
     description='A package for converting video poses to GeckoLib4 animations',
     long_description=open('README.md').read() if os.path.exists('README.md') else '',
     long_description_content_type='text/markdown',
-    url='https://github.com/Jaffe2718/video2geckolib4',  # 替换为你的项目仓库地址
+    url='https://github.com/Jaffe2718/video2geckolib4',
     packages=find_packages(include=['video2geckolib4']),
+    package_data={
+        'video2geckolib4': ['basemodel.bbmodel']
+    },
     install_requires=[
         'opencv-contrib-python',
         'mediapipe',
--- video2geckolib4/__init__.py
+++ video2geckolib4/__init__.py
@@ -11,12 +11,13 @@ from .pose_converter import PoseConverter, BONES
 __version__ = '0.1.0'
 __all__ = ['VideoPoseEstimator', 'PoseConverter', 'AnimationBuilder', 'AnimationSet', 'BONES', "auto_estimate"]
 
+
 def auto_estimate(videos: Iterable[str | Path],
-                   out_json: str | Path,
-                   sample_fps: float = 20.0,
-                   model_complexity: Literal[0, 1, 2] = 1,
-                   smooth: bool = True,
-                   allow_translate: bool = False):
+                  out_json: str | Path,
+                  sample_fps: float = 20.0,
+                  model_complexity: Literal[0, 1, 2] = 1,
+                  smooth: bool = True,
+                  allow_translate: bool = False):
     """
     Estimate pose for all videos and merge them into a single animation json file, each video will be converted into a
      single animation which will be named same as the video file basename
@@ -33,9 +34,10 @@ def auto_estimate(videos: Iterable[str | Path],
         # open video
         cap = cv2.VideoCapture(vpath)
         video_fps = cap.get(cv2.CAP_PROP_FPS)
-        animation_len = cap.get(cv2.CAP_PROP_FRAME_COUNT) / video_fps    # unit: seconds
+        animation_len = cap.get(cv2.CAP_PROP_FRAME_COUNT) / video_fps  # unit: seconds
         cap.release()
-        world_landmark_frames, landmark_frames = vpe.estimate_timestamp(vpath, [i / sample_fps for i in range(int(animation_len * sample_fps))])
+        world_landmark_frames, landmark_frames = vpe.estimate_timestamp(vpath, [i / sample_fps for i in
+                                                                                range(int(animation_len * sample_fps))])
         animation_builder = AnimationBuilder(os.path.basename(vpath).split('.')[0], animation_len)
         for i, frame in enumerate(PoseConverter.calculate_poses(world_landmark_frames, smooth)):
             for bone in BONES:
@@ -47,14 +49,12 @@ def auto_estimate(videos: Iterable[str | Path],
     animation_set.save(out_json)
 
 
-
-
 def gen_basemodel(out: str | Path):
     """
     copy base model to given path
     :param out:  to output directory
     """
-    bbmodel = pkg_resources.files('video2geckolib4') /'base_model.bbmodel'
+    bbmodel = pkg_resources.files('video2geckolib4') / 'basemodel.bbmodel'
     with bbmodel.open("rb") as f:
         with open(out, "wb") as o:
             o.write(f.read())
